76b13d085de9c6f2680f8a6824c0c556857645a1
Aftab mohammed k <87110394+aftabmk@users.noreply.github.com>
2022-10-16 14:08:24 +0530

    Create epoch.js

---
+++ epoch.js
@@ -0,0 +1,6 @@
+  var currentTime   = "21/02/2021 12:00"
+  var monthPosition = currentTime.split('/', 2).join('/').length
+  var day           = currentTime.substring(0, currentTime.indexOf('/'))
+  var month         = currentTime.substring(currentTime.indexOf('/') + 1, monthPosition);
+  var time          = month + "/" + day + currentTime.substring(month.length + day.length  + 1); 
+//"02/21/2021 12:00"
